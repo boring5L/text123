ce55c8f0e8c1ba1698cab8ecff98428c755e88d9
Boring <1830942448@qq.com>
2024-11-28 16:15:53 +0800

    zhecaishicstx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">will-change not go to where sitll sitll {{ count }}</button>
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">count is ****+++{{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
