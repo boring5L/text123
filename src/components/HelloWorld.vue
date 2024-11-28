6f17bb07e29a68e79504b7d547010c4e17379c32
Boring <1830942448@qq.com>
2024-11-28 16:03:03 +0800

    123456

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count this very new  123 {{ count }}</button>
+    <button type="button" @click="count++">will-change not go to where {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
