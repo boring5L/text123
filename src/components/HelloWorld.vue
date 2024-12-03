65deb69a8d2b79c9121a94aa23a0b4cf7f05e88b
Boring <1830942448@qq.com>
2024-12-03 15:03:40 +0800

    TJ

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,17 +9,21 @@ const count = ref(0)
 </script>
 
 <template>
+  <div class="rad-a">
+     <div class="rad-b">
+     </div>
+     <div class="rad-c">
+     </div>
+     <div class="rad-d">
+     </div>
+  </div>
   <h1>{{ msg }}</h1>
-
+  <h1>{{ msg }}</h1><h1>{{ msg }}</h1><h1>{{ msg }}</h1>
   <div class="card">
-<<<<<<< HEAD
-    <button type="button" @click="count++">will-change not go to where {{ count }}</button>
-=======
-    <button type="button" @click="count++">count is master imaster {{ count }}</button>
->>>>>>> parent of c2db33b (text)
+    <button type="button" @click="count++"> {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code>
     </p>
   </div>
 
@@ -37,6 +41,35 @@ const count = ref(0)
       >Vue Docs Scaling up Guide</a
     >.
   </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
@@ -44,4 +77,39 @@ const count = ref(0)
 .read-the-docs {
   color: #888;
 }
+
+
+
+
+.rad-a{
+  justify-content: center;
+  display: flex;
+  background-color: aqua;
+  width: 250px;
+  height: 50px;
+  align-items: flex-end;
+  justify-content: space-between;
+  .rad-b{
+    display: flex;
+    /* margin-left: 60px; */
+    background-color: red;
+    width: 50px;
+    height: 50px;
+  }
+  .rad-c{
+    display: flex;
+    background-color: blue;
+    width: 50px;
+    height: 50px;
+  }
+  .rad-d{
+    background-color: blueviolet;
+    /* position: absolute; */
+    display: flex;
+    width: 50px;
+    height: 50px;
+
+  }
+}
+
 </style>
